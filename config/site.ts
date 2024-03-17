ccba23fd03004090370f64529cb2fe7a63eedda0
highfreshness <meteo0718@gmail.com>
2024-03-17 23:34:00 +0900

    📝update title tag

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Highfreshness-Blog",
+  name: "Highfreshness Blog",
   url: "https://example.com",
   description: "Nextjs 14 blog using velite, tailwind and shadcn",
   author: "Highfreshness",
